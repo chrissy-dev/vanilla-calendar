e05dfc0c5139ea7f76089149833101a3bec60677
Alper Kilci <alperkilci@gmail.com>
2018-02-03 15:12:33 +0100

    [fix] change error level of max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -93,7 +93,7 @@ module.exports = {
         "lines-around-directive": "error",
         "lines-between-class-members": "error",
         "max-depth": "error",
-        "max-len": "error",
+        "max-len": "warn",
         "max-lines": "error",
         "max-nested-callbacks": "error",
         "max-params": "error",
